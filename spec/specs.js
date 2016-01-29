aec8a9eb65818ea00d4e38b60739f055d02ae985
Oskar <oskarradonkimball@gmail.com>
2016-01-29 08:05:05 -0800

    Add second passing spec

--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,7 @@
 describe ("Pizza", function() {
-  it ("will generate pizza price based on pizza size", function () {
-    var testPizza = new Pizza("large");
+  it ("will generate pizza price based on pizza size and toppings", function () {
+    var testPizza = new Pizza("large", 5);
     expect(testPizza.size).to.equal("large");
+    expect(testPizza.toppings).to.equal(5);
   });
 });
